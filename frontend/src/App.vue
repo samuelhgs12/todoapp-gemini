04cff151f283913e1eb138ddca6973ebf2ab59cb
samuelhgs12 <samuel.sousa@ufpi.edu.br>
2025-12-18 18:51:02 -0300

    feat: scaffold frontend with Vite and TypeScript

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// Este é o ponto de partida para os componentes da sua aplicação.
+</script>
+
+<template>
+  <div>
+    <h1>TodoApp</h1>
+  </div>
+</template>
+
+<style scoped>
+/* Estilos para este componente */
+</style>
